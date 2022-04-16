1590c517e49500d7d1babcc6986336e544d0368f
Adrián López García de Lomana <14022465+adelomana@users.noreply.github.com>
2022-04-16 22:46:43 +0100

    solved M1 parallelisation

--- rmarinus/carotenoid_search.py
+++ rmarinus/carotenoid_search.py
@@ -1,8 +1,7 @@
-import numpy, pandas, datetime
-import cobra, cobra.test
+import numpy, datetime, cobra, multiprocessing, pandas, pickle
 
-import multiprocessing, multiprocessing.pool
-from multiprocessing import Process, Queue
+#import warnings
+#warnings.filterwarnings("ignore")
 
 def growth_coupled_analysis(task):
 
@@ -49,42 +48,53 @@ def printt(message):
 ### MAIN
 ###
 
-# 1. load model
-model = cobra.io.read_sbml_model("/Users/adrian/scratch/rmarinus/Rmarinus_578_model.xml")
-print(model.summary())
+# usage: time python carotenoid_search.py &> carotenoid_search_messages.txt
 
-# 2. get info about model
-wt_solution = model.optimize()
-print(wt_solution.objective_value)
+# 100 x 100 genes = 4,950 pairs. @ M1 takes 21 sec
+# 250 x 250 genes = 31,125 pairs. @ M1 takes 2 min
+# 578 x 578 genes = 166,753 pairs. @ M1 should take 11 min. It took 12
 
-number_of_genes = len(model.genes)
-print(number_of_genes)
+# 6. parallel computation
+if __name__ == '__main__':
 
-# 3. create demand reaction
-model.add_boundary(model.metabolites.get_by_id("CAROT_RMAR_c"), type="demand")
-print("demands", model.demands)
-for reaction in model.demands:
-    print(reaction)
+    # 0. user-defined variables
+    number_of_threads = 8
 
-# 4. growth-coupled design
-reaction_of_interest = 'DM_CAROT_RMAR_c'
-biomass_reaction_label = 'BIOMASS'
+    # 1. load the model
+    model = cobra.io.read_sbml_model("/Users/adrian/scratch/rmarinus/Rmarinus_578_model.xml")
+    number_of_genes = len(model.genes)
 
-# 4.1. run in a paralel environment
-number_of_threads = 2
+    # 2. solve the model
+    wt_solution = model.optimize()
 
-printt('working with {} genes'.format(number_of_genes))
+    # 3. add demand reaction
+    model.add_boundary(model.metabolites.get_by_id("CAROT_RMAR_c"), type="demand")
 
-tasks = []
-for i in range(len(model.genes[:5])):
-    for j in range(len(model.genes[:5])):
-        if i < j:
-            task = [i, j, reaction_of_interest, biomass_reaction_label, model]
-            tasks.append(task)
-printt('working with {} gene pairs'.format(len(tasks)))
+    # 4. growth-coupled design
+    reaction_of_interest = 'DM_CAROT_RMAR_c'
+    biomass_reaction_label = 'BIOMASS'
 
-printt('entering a parallel world of {} threads'.format(number_of_threads))
-hydra = multiprocessing.pool.Pool(number_of_threads)
-hydra_output = hydra.map(growth_coupled_analysis, tasks)
-hydra.close()
-printt('completed {} tasks'.format(len(hydra_output)))
+    # 5. define tasks
+    printt('working with {} genes'.format(number_of_genes))
+    tasks = []
+    for i in range(len(model.genes)):
+        for j in range(len(model.genes)):
+            if i < j:
+                task = [i, j, reaction_of_interest, biomass_reaction_label, model]
+                tasks.append(task)
+    printt('working with {} gene pairs'.format(len(tasks)))
+
+    # 6. run in parallel
+    with multiprocessing.Pool(number_of_threads) as hydra:
+        hydra_output = hydra.map(growth_coupled_analysis, tasks)
+    printt('obtained {} results'.format(len(hydra_output)))
+
+    # 7. store results as a dataframe, then pickle it
+    df = pandas.DataFrame(hydra_output, columns=['i', 'j', 'KO growth', 'min production', 'max production'])
+    df.sort_values(by=['min production'], ascending=False, inplace=True)
+    print(df.head())
+
+    printt('store double KO information dataframe as a pickle')
+    f = open('doubleKO.pickle','wb')
+    pickle.dump(df, f)
+    f.close()
